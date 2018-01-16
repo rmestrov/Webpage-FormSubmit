6ff8c2d96d234eb10c9faab1e0aaad109f8a1ff0
rmestrov <rmestrov@fesb.hr>
2018-01-16 21:57:45 +0100

    Added regex pattern for validation

--- js/validation.js
+++ js/validation.js
@@ -4,14 +4,24 @@ $(document).ready(function () {
         var name = document.getElementById('name').value;
         var email = document.getElementById('email').value;
         var message = document.getElementById('message').value;
+        var regexName = /^[a-zA-Z\s]*$/; 
+        var regexEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
 
         if (name.length == 0) {
             alert("Please enter your name");
             event.preventDefault();
         }
+        else if(regexName.test(name) == false) {
+            alert("Please enter a valid name");
+            event.preventDefault();
+        }
         
         else if (email.length == 0) {
             alert("Please enter your email");
+            event.preventDefault();
+        }
+        else if(regexEmail.test(email) == false) {
+            alert("Please enter a valid email");
             event.preventDefault();
         }
         
