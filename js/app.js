34dfea2d87b215fd0f7f2b66d8a3798909461070
rmestrov <rmestrov@fesb.hr>
2018-01-16 21:09:31 +0100

    Added post method for formSubmit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 require('./menu.js');
 require('./slider.js');
-require('./validation.js');
 require('../style/main.css');
 require('../style/style-mobile.css');
 require('../style/style-tablet.css');
+require('./validation.js');
--- js/validation.js
+++ js/validation.js
@@ -1,19 +1,35 @@
 $(document).ready(function () {
     $("#submit").click(function () {
+        
         var name = document.getElementById('name').value;
         var email = document.getElementById('email').value;
         var message = document.getElementById('message').value;
 
         if (name.length == 0) {
             alert("Please enter your name");
+            event.preventDefault();
         }
         
         else if (email.length == 0) {
             alert("Please enter your email");
+            event.preventDefault();
         }
         
         else if (message.length == 0) {
             alert("Please enter a message");
+            event.preventDefault();
         }
-    });
+
+       $("#formSubmit").submit(function(event) {
+           event.preventDefault();
+           var $form = $(this);
+           url = $form.attr('action');
+           var posting = $.post(
+               url, {
+                   contactname: $('#name').val(),
+                   emailaddress: $('#email').val(),
+                   message: $('#message').val()
+                });
+            });
+        });
 });
